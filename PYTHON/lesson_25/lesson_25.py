9a32b73006118bdb1b395697e825aeb97c70771f
VladimirMonin <moninvladimir88@gmail.com>
2026-07-27 23:06:04 +0500

    lesson_25: Вернулись к BaseConfig для логирования но хендлеры создали отдельно - чтобы в файл и в терминал были разные уровени логирования

--- PYTHON/lesson_25/lesson_25.py
+++ PYTHON/lesson_25/lesson_25.py
@@ -9,45 +9,32 @@
 import logging
 from utils import fun_foo, divide_foo
 
-# 1. Общий корневой логгер
-root_logger = logging.getLogger()
 
-# 2. Указываю минимальный уровень сообщений
-root_logger.setLevel(logging.DEBUG)
-
-# 3. Описываем внешний вид сообщений
-formatter = logging.Formatter(
-    fmt="%(levelname)s | %(asctime)s | %(name)s | %(message)s"
-)
-
-# 4. Создаем обработчик для терминала
+# Обработчик терминала
 console_handler = logging.StreamHandler()
-console_handler.setLevel(logging.DEBUG)
+console_handler.setLevel(logging.INFO)
 
-# 5. Передаем ему формат в котором он должен работать
-console_handler.setFormatter(formatter)
 
-#6. Подключаем обработкчик к общему логгеру
-root_logger.addHandler(console_handler)
-
-# 7. Создаем filehandler Для записи логов в файл
+# Обработчик файла
 file_handler = logging.FileHandler(
     filename="lesson_25.log",
-    mode="w",
-    encoding="utf-8"
+    encoding="utf-8",
+)
+file_handler.setLevel(logging.DEBUG)
 
+
+# Общая настройка логирования
+logging.basicConfig(
+    level=logging.DEBUG,
+    format="%(levelname)s | %(asctime)s | %(name)s | %(message)s",
+    handlers=[
+        console_handler,
+        file_handler,
+    ],
 )
 
-# 8. Указываем форматтер и подклчаем к логгеру
-file_handler.setFormatter(formatter)
-root_logger.addHandler(file_handler)
 
-
-
-#9. Создаем логгер для модуля
 logger = logging.getLogger(__name__)
-
-
 
 
 logger.info("Программа запущена")
