927ae9a5748722c7e1b04d700d7643e3f21d29df
VladimirMonin <moninvladimir88@gmail.com>
2026-07-27 23:27:15 +0500

    lesson_25: TimedRotatingFileHandler - разборали ротацию по времени - примеры на день, неделю, месяц и т.п.

--- PYTHON/lesson_25/lesson_25.py
+++ PYTHON/lesson_25/lesson_25.py
@@ -8,19 +8,38 @@
 
 import logging
 from utils import fun_foo, divide_foo
-from logging.handlers import RotatingFileHandler
+from logging.handlers import TimedRotatingFileHandler
 
 # Обработчик терминала
 console_handler = logging.StreamHandler()
 console_handler.setLevel(logging.INFO)
 
+"""
+"S"	Секунды
+"M"	Минуты
+"H"	Часы
+"D"	Дни
+"midnight"	Каждую полночь
+"W0"	Каждый понедельник
+"W1"	Каждый вторник
+"W6"	Каждое воскресенье
+
+Раз в месяц будет вот так выглядить
+    when="D",
+    interval=30,
+    backupCount=12,
+    encoding="utf-8",
+)
+"""
+
 
 # Обработчик файла
-file_handler = RotatingFileHandler(
+file_handler = TimedRotatingFileHandler(
     filename="lesson_25.log",
+    when="midnight",
+    interval=1,
+    backupCount=7,
     encoding="utf-8",
-    maxBytes=1_000_000,
-    backupCount=3
 )
 
 file_handler.setLevel(logging.DEBUG)
@@ -51,5 +70,5 @@ b = int(input("Введите число b"))
 divide_foo(a, b)
 
 
-for number in range(1000):
-    logger.debug(f"Отладочное сообщение №{number} кот чихнул!")
+# for number in range(1000):
+#     logger.debug(f"Отладочное сообщение №{number} кот чихнул!")
