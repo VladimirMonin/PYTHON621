8305c5dcbd21b9ec5b4e109c49a6a61c44f71e3d
VladimirMonin <moninvladimir88@gmail.com>
2026-07-29 22:08:02 +0500

    lesson_26: проблема множественных assert в тестовых функциях - Первый упавший assert - это упавший тест - Что делать?!

--- test_lesson_26.py
+++ test_lesson_26.py
@@ -2,7 +2,10 @@ from lesson_26 import divide_foo
 import pytest
 
 def test_positiv_divide_foo():
+    assert divide_foo(12, 3) == 3.0
     assert divide_foo(10, 2) == 5.0
+    assert divide_foo(9, 3) == 3.0
+    
 
 
 def test_zero_divide_foo():
