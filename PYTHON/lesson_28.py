e9a41320acf8fd5a53776896ae252897ec2a3483
VladimirMonin <moninvladimir88@gmail.com>
2026-08-19 22:39:22 +0500

    lesson_28: Класс Item представляющий одну товариную позицию в заказе

--- PYTHON/lesson_28.py
+++ PYTHON/lesson_28.py
@@ -6,53 +6,32 @@
 - Аттрибуты класса
 """
 
-"""Заказ = Словарь
-Данные между функцями передаются
-через словари
 
-create_order()
-add_item()
-calculate_total()"""
+class Item:
+    """
+    Класс представляющий товарную позицию в корзине
+    """
 
-class Order:
-    available_regions = ["Шир", "Лихолесье", "Мордор"]
+    def __init__(self, name: str, price: float, category: str, quantity: int):
+        self.name = name
+        self.price = price
+        self.category = category
+        self.quantity = quantity
+        self.total_price: float = self.price * self.quantity
 
-    def __init__(self, customer_name: str, region: str):
-        self.customer_name = customer_name
-        self.items: list = []
-        self.total: int = 0
-        self.region = region
-
-    def add_item(self, position: str, amount: int):
-        self.items.append({"position": position, "amount": amount})
-        self.total += amount
-
-    def set_region(self, region: str):
-        if region in self.available_regions:
-            self.region = region
-        else:
-            raise ValueError("Недопустимый регион")
+    def __str__(self):
+        return f"{self.name} - {self.price} руб., {self.quantity} шт. Общая стоимость: {self.total_price} руб."
 
 
-    @classmethod
-    def set_available_regions(cls, regions: list):
-        cls.available_regions = regions
 
-    @staticmethod
-    def validate_phone(phone: str) -> bool:
-        return len(phone) == 10 and phone.isdigit()
+item_1 = Item("Чебурек", 100, "Выпечка", 5)
+item_2 = Item("Котлета", 50, "Мясные блюда", 2)
+print(item_1)
+print(item_2)
 
+data_item_3 = {"name": "Пюре", "price": 40, "category": "Гарнир", "quantity": 1}
+# item_3 = Item(name=data_item_3["name"], price=data_item_3["price"], category=data_item_3["category"], quantity=data_item_3["quantity"])
 
-order_1 = Order("Фёдор Сумкин", "Шир")
-Order.set_available_regions(["Шир", "Лихолесье", "Мордор", "Южное Бутово"])
-order_1.set_region("Южное Бутово")
-order_1.add_item("Чехол для кольца всевластия", 1)
-order_1.add_item("Удобные тапочки", 1)
+item_3 = Item(**data_item_3)
+print(item_3)
 
-print("Имя клиента:", order_1.customer_name)
-print("Товары:", order_1.items)
-print("Общая стоимость:", order_1.total)
-print("Регион:", order_1.region)
-
-print(order_1.validate_phone("1234567890"))
-print(Order.validate_phone("1234567890"))
