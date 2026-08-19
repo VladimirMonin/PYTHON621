573fb770b7384b28f19f429e6e4dc888d74c5d42
VladimirMonin <moninvladimir88@gmail.com>
2026-08-19 21:14:43 +0500

    lesson_28: Повторили self и init - Написали класс заказа

---
+++ PYTHON/lesson_28.py
@@ -0,0 +1,34 @@
+"""
+Занятие №28: Анатомия класса и связи между объектами
+- Методы экземпляра
+- Методы класса
+- Аттрибуты экземпляра
+- Аттрибуты класса
+"""
+
+"""Заказ = Словарь
+Данные между функцями передаются
+через словари
+
+create_order()
+add_item()
+calculate_total()"""
+
+class Order:
+    def __init__(self, customer_name: str):
+        self.customer_name = customer_name
+        self.items: list = []
+        self.total: int = 0
+
+    def add_item(self, position: str, amount: int):
+        self.items.append({"position": position, "amount": amount})
+        self.total += amount
+
+
+order_1 = Order("Фёдор Сумкин")
+order_1.add_item("Чехол для кольца всевластия", 1)
+order_1.add_item("Удобные тапочки", 1)
+
+print("Имя клиента:", order_1.customer_name)
+print("Товары:", order_1.items)
+print("Общая стоимость:", order_1.total)
