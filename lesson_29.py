7b9a9da7108d495016883e15134d16b90ce3b4e6
VladimirMonin <moninvladimir88@gmail.com>
2026-08-24 22:06:50 +0500

    lesson_29: геттер и сеттер через property для установки и чтения максимальной скорости конкретного автомобиля

--- lesson_29.py
+++ lesson_29.py
@@ -9,8 +9,8 @@ __dict__
 
 
 class Car:
-    min_speed = 0
-    max_speed = 500
+    min_speed_limit = 0
+    max_speed_limit = 500
 
     def __init__(self, model: str, max_speed: int):
         self.model = model
@@ -33,16 +33,20 @@ class Car:
     def __str__(self):
         return f"Модель {self.model}, Максимальная скорость {self.__max_speed} км/ч"
 
-    def set_max_speed(self, new_speed: int):
-        if self.min_speed < new_speed < self.max_speed:
+    @property
+    def max_speed(self):
+            return self.__max_speed
+
+    @max_speed.setter
+    def max_speed(self, new_speed: int):
+        if self.min_speed_limit < new_speed < self.max_speed_limit:
             self.__max_speed = new_speed
         else:
             raise ValueError(
-                f"Максимальная скорость должна быть в диапазоне от {self.min_speed} до {self.max_speed} км/ч"
+                f"Максимальная скорость должна быть в диапазоне от {self.min_speed_limit} до {self.max_speed_limit} км/ч"
             )
 
-    def get_max_speed(self):
-        return self.__max_speed
+    
 
 
 car1 = Car("Лада Малина", 200)
@@ -59,5 +63,7 @@ print(car1)
 print(car1)
 # car1.start()
 
-car1.set_max_speed(450)
+car1.max_speed = 300
+print(car1.max_speed)
+
 print(car1)
