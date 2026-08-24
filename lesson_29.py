1eda15de53e1fd2ea5a77f7f98374f3acb8dd7ec
VladimirMonin <moninvladimir88@gmail.com>
2026-08-24 21:36:00 +0500

    lesson_29: добавили атрибуты класса с ограничителями - Добавили метод "сеттер" для установки максимальной скорости - Довили метод "геттер" для чтения данных с поля максимальной скорости

--- lesson_29.py
+++ lesson_29.py
@@ -9,11 +9,13 @@ __dict__
 
 
 class Car:
+    min_speed = 0
+    max_speed = 500
+
     def __init__(self, model: str, max_speed: int):
         self.model = model
         self.__max_speed = max_speed
         self.speed = 0
-
 
     def start(self):
         self.gas_controller()
@@ -29,8 +31,18 @@ class Car:
         print("Управление системой подачи топливом")
 
     def __str__(self):
-        return(f"Модель {self.model}, Максимальная скорость {self.__max_speed} км/ч")
+        return f"Модель {self.model}, Максимальная скорость {self.__max_speed} км/ч"
 
+    def set_max_speed(self, new_speed: int):
+        if self.min_speed < new_speed < self.max_speed:
+            self.__max_speed = new_speed
+        else:
+            raise ValueError(
+                f"Максимальная скорость должна быть в диапазоне от {self.min_speed} до {self.max_speed} км/ч"
+            )
+
+    def get_max_speed(self):
+        return self.__max_speed
 
 
 car1 = Car("Лада Малина", 200)
@@ -43,7 +55,9 @@ print("Максимальная скорость автомобиля", car1._Ca
 # car1.__gas_controller()
 print(car1)
 
-car1.__max_speed = 400
+# car1.__max_speed = 400
 print(car1)
-car1.start()
+# car1.start()
 
+car1.set_max_speed(450)
+print(car1)
