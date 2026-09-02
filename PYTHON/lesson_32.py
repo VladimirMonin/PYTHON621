0ddbbe2ca9d88cf9caaec6e783492f60793b5ad3
VladimirMonin <moninvladimir88@gmail.com>
2026-09-02 22:44:56 +0500

    lesson_32: написали цепочку наследования и посмотрели mro - в каком порядке пайтон будет искать методы у этих классов

--- PYTHON/lesson_32.py
+++ PYTHON/lesson_32.py
@@ -65,3 +65,30 @@ print(jpeg_file is jpeg_file_2)  # False
 
 print(jpeg_file == jpeg_file)  # True
 print(jpeg_file is jpeg_file)  # True
+
+
+class BigMatryoshka:
+    def __init__(self, name):
+        self.big_name = name
+
+    def __str__(self):
+        return f"Имя: {self.big_name}"
+
+
+class MediumMatryoshka(BigMatryoshka):
+    def __init__(self, name):
+        super().__init__(name)
+        self.medium_name = name
+
+
+class SmallMatryoshka(MediumMatryoshka):
+    def __init__(self, name):
+        super().__init__(name)
+        self.small_name = name
+
+
+small_matryoska = SmallMatryoshka("Байден Неволяшка")
+print(small_matryoska.big_name)  # Байден Неволяшка
+
+# Сделаем проверку на MRO (Method Resolution Order)
+print(small_matryoska.__class__.__mro__)  # (<class '__main__.SmallMatryoshka'>, <class '__main__.MediumMatryoshka'>, <class '__main__.BigMatryoshka'>, <class 'object'>)
