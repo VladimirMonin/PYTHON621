5b422aeb610525b0b6d52ff541aaf4bff17b192c
VladimirMonin <moninvladimir88@gmail.com>
2026-09-02 23:28:38 +0500

    lesson_32: Вариант миксина с матрёшкой, где инициализаторы вызываются вручную и не совсем правильно описан финальный класс.

--- PYTHON/lesson_32.py
+++ PYTHON/lesson_32.py
@@ -5,90 +5,52 @@ Lesson 32: Абстрактные классы
 - Кооперативный `super()` и mixin-классы
 """
 
-from abc import ABC, abstractmethod
 
-
-class AbstractImageFile(ABC):
-    def __init__(self, file_path):
-        self.file_path = file_path
-
-    @abstractmethod
-    def read(self):
-        pass
-
-    @abstractmethod
-    def crop(self):
-        pass
+class Matryoska:
+    def __init__(self, name):
+        self.name = name
 
     def __str__(self):
-        return f"Файл {self.__class__.__name__} с путем {self.file_path}"
-
-    def __eq__(self, other):
-        if isinstance(other, AbstractImageFile):
-            return self.file_path == other.file_path
-        return False
+        return f"Матрешка {self.name}"
 
 
-class JpegImageFile(AbstractImageFile):
-    def read(self):
-        print("Чтение JPEG файла")
-
-    def crop(self):
-        print("Обрезка JPEG файла")
-
-
-class PngImageFile(AbstractImageFile):
-    def read(self):
-        print("Чтение PNG файла")
-
-    def crop(self):
-        print("Обрезка PNG файла")
-
-
-a_str = "один"
-b_str = "один"
-c_str = "два"
-
-print(a_str == b_str)  # True
-print(a_str is b_str)  # True
-print(a_str == c_str)  # False
-print(a_str is c_str)  # False
-
-jpeg_file = JpegImageFile("image.jpeg")
-jpeg_file_2 = JpegImageFile("image.jpeg")
-png_file = PngImageFile("image.png")
-
-print(
-    jpeg_file == jpeg_file_2
-)  # False НО ЕСЛИ БУДЕТ eq который сверяет по атрибутам, то будет True
-print(jpeg_file is jpeg_file_2)  # False
-
-print(jpeg_file == jpeg_file)  # True
-print(jpeg_file is jpeg_file)  # True
-
-
-class BigMatryoshka:
-    def __init__(self, name):
-        self.big_name = name
+class MaterialMetalMixin:
+    def __init__(self):
+        self.material = "металл"
 
     def __str__(self):
-        return f"Имя: {self.big_name}"
+        return f"Матрешка {self.name} из {self.material}"
 
 
-class MediumMatryoshka(BigMatryoshka):
+class TolkingMixin:
+    def __init__(self):
+        self.feat = "говорит"
+        self.phrase = "Сделаем Америку Грейт Агейн!"
+
+    def change_words(self, new_phrase):
+        self.phrase = new_phrase
+
+    def voice_bottom(self):
+        return f"{self.name} {self.feat}: {self.phrase}"
+
+    def __str__(self):
+        return f"Матрешка {self.name} {self.feat}: {self.phrase}"
+
+
+class MetallicMatryoska(Matryoska, MaterialMetalMixin, TolkingMixin):
     def __init__(self, name):
-        super().__init__(name)
-        self.medium_name = name
+        Matryoska.__init__(self, name)
+        MaterialMetalMixin.__init__(self)
+        TolkingMixin.__init__(self)
+
+    def __str__(self):
+        return f"Матрешка {self.name} из {self.material} {self.feat}: {self.phrase}"
 
 
-class SmallMatryoshka(MediumMatryoshka):
-    def __init__(self, name):
-        super().__init__(name)
-        self.small_name = name
+tramp_matryoska = MetallicMatryoska("Трамп")
+tramp_matryoska.change_words("Make America Great Again!")
+print(tramp_matryoska.voice_bottom())
 
-
-small_matryoska = SmallMatryoshka("Байден Неволяшка")
-print(small_matryoska.big_name)  # Байден Неволяшка
-
-# Сделаем проверку на MRO (Method Resolution Order)
-print(small_matryoska.__class__.__mro__)  # (<class '__main__.SmallMatryoshka'>, <class '__main__.MediumMatryoshka'>, <class '__main__.BigMatryoshka'>, <class 'object'>)
+medved_matryoska = MetallicMatryoska("Медведев")
+medved_matryoska.change_words("Денег нет, но вы держитесь!")
+print(medved_matryoska.voice_bottom())
