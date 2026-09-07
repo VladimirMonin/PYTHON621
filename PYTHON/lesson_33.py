f0231789f6a9f153b27beb42a0eb2bc9fa84bd89
VladimirMonin <moninvladimir88@gmail.com>
2026-09-07 21:20:46 +0500

    lesson_33: собственные методы сериализации - Описали свои методы для сериализации в словарь из словаря - Посмотрели как работает repr (сделали свой - просто как пример того что внутри @dataclasses)

--- PYTHON/lesson_33.py
+++ PYTHON/lesson_33.py
@@ -4,6 +4,7 @@ Lesson 33: Python Dataclasses
 
 from dataclasses import dataclass, field
 
+
 @dataclass
 class Employee:
     name: str
@@ -11,19 +12,51 @@ class Employee:
     position: str
     salary: float
 
+    def __str__(self):
+        return f"Сотрудник: {self.name}, Возраст: {self.age}, Должность: {self.position}, Зарплата: {self.salary}"
+
+    def __repr__(self):
+        """
+        Писать его нет смысла - это делает @dataclass - выглядит это примерно так
+        Это служебное представление объекта удобное для сериализации
+        """
+        return f"Employee(name='{self.name}', age={self.age}, position='{self.position}', salary={self.salary})"
+
+    @classmethod
+    def from_dict(cls, data: dict):
+        return cls(**data)
+
+    def to_dict(self):
+        return self.__dict__
+
 
 employee_1 = Employee("Шарик", 2, "Пес", 1000.0)
 employee_2 = Employee("Матроскин", 3, "Кот", 5000.0)
 
 # Сбросить это в строку сериализация
-employee_1_str = str(employee_1)
+employee_1_str = repr(employee_1)
 print(employee_1_str)
 
 # Обратно в объект десериализация
 employee_1_back = eval(employee_1_str)
 
 
-
 print(employee_1)
 print(employee_2)
 
+
+employee_dict_3 = {
+    "name": "Дядя Фёдор",
+    "age": 12,
+    "position": "Мальчик",
+    "salary": 15000.0,
+}
+
+employee_dict_4 = {"name": "Заяц", "age": 2, "position": "Фото-дичь", "salary": 0.0}
+
+employee_3 = Employee.from_dict(employee_dict_3)
+employee_4 = Employee.from_dict(employee_dict_4)
+
+dict_4 = employee_4.to_dict()
+
+print(dict_4)
