4a1e42f7b80dc71f28f55a84401768a899c21c62
VladimirMonin <moninvladimir88@gmail.com>
2026-09-07 22:56:47 +0500

    lesson_33: Познакомились с параметрами по-умолчанию в dataclass а так же с default_factory

--- PYTHON/lesson_33.py
+++ PYTHON/lesson_33.py
@@ -9,18 +9,12 @@ from dataclasses import dataclass, field
 class Employee:
     name: str
     age: int
-    position: str
-    salary: float
+    skills: list = field(default_factory=list)
+    position: str = "Безработный"
+    salary: float = 0.0
 
     def __str__(self):
-        return f"Сотрудник: {self.name}, Возраст: {self.age}, Должность: {self.position}, Зарплата: {self.salary}"
-
-    def __repr__(self):
-        """
-        Писать его нет смысла - это делает @dataclass - выглядит это примерно так
-        Это служебное представление объекта удобное для сериализации
-        """
-        return f"Employee(name='{self.name}', age={self.age}, position='{self.position}', salary={self.salary})"
+        return f"Сотрудник: {self.name}, Возраст: {self.age}, Должность: {self.position}, Зарплата: {self.salary}\nSkills: {self.skills}"
 
     @classmethod
     def from_dict(cls, data: dict):
@@ -30,19 +24,21 @@ class Employee:
         return self.__dict__
 
 
-employee_1 = Employee("Шарик", 2, "Пес", 1000.0)
-employee_2 = Employee("Матроскин", 3, "Кот", 5000.0)
+employee_dict_1 = {
+    "name": "Шарик",
+    "age": 2,
+    "position": "Пес",
+    "salary": 1000.0,
+    "skills": ["жаловаться", "фотографировать", "покупать кеды на Озон"],
+}
 
-# Сбросить это в строку сериализация
-employee_1_str = repr(employee_1)
-print(employee_1_str)
-
-# Обратно в объект десериализация
-employee_1_back = eval(employee_1_str)
-
-
-print(employee_1)
-print(employee_2)
+employee_dict_2 = {
+    "name": "Матроскин",
+    "age": 3,
+    "position": "Кот",
+    "salary": 5000.0,
+    "skills": ["манипулировать Шариком", "пить молоко"],
+}
 
 
 employee_dict_3 = {
@@ -50,37 +46,32 @@ employee_dict_3 = {
     "age": 12,
     "position": "Мальчик",
     "salary": 15000.0,
+    "skills": ["сбегать от родителей", "неправильно есть бутерброды"],
 }
 
-employee_dict_4 = {"name": "Заяц", "age": 2, "position": "Фото-дичь", "salary": 0.0}
-
-employee_3 = Employee.from_dict(employee_dict_3)
-employee_4 = Employee.from_dict(employee_dict_4)
-
-dict_4 = employee_4.to_dict()
-
-print(dict_4)
-
-# Список персонажей
-list_of_employees = [employee_1, employee_2, employee_3, employee_4]
-
-# Сбрасываю их в JSON файл
-import json
-FILE = "employees.json"
-
-dicts_list_of_employees = [employee.to_dict() for employee in list_of_employees]
+employee_dict_4 = {
+    "name": "Заяц",
+    "age": 2,
+    "position": "Фото-дичь",
+    "salary": 0.0,
+    "skills": ["позировать на фото", "убегать"],
+}
 
 
-with open(FILE, "w", encoding="utf-8") as file:
-    json.dump(dicts_list_of_employees, file, indent=4, ensure_ascii=False)
+employee_1 = Employee.from_dict(employee_dict_1)
+
+print(employee_1)
+
+employee_2 = Employee.from_dict(employee_dict_2)
+print(employee_2)
+print(employee_1)
 
 
-# А теперь обратно!
+employee_dict_5 = {"name": "Печкин", "age": 45}
 
-with open(FILE, "r", encoding="utf-8") as file:
-    data = json.load(file)
+employee_5 = Employee(**employee_dict_5)
+print(employee_5)
 
-list_of_employees_from_json = [Employee.from_dict(employee_dict) for employee_dict in data]
+employee_5.skills.append("Езда на велосипеде")
+print(employee_5)
 
-print(list_of_employees_from_json)
-[print(employee) for employee in list_of_employees_from_json]
