922d14b6baf9231196d2dec2d346a084e47fef90
VladimirMonin <moninvladimir88@gmail.com>
2026-09-09 23:39:49 +0500

    lesson_34: Познакомились с библиотекой `pydantic-settings` и её служебным классом `BaseSettings`. Посмотрели на автоматическую подгрузку переменных окружения и конвертацию данных.

---
+++ lesson_34_settings.py
@@ -0,0 +1,18 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    polza_api_key: str
+    minute_rate_limit: int
+    is_retry: bool
+    retry_rate: int
+
+    model_config = SettingsConfigDict(
+        env_file=".env",
+        env_file_encoding="utf-8",
+
+    )
+
+settings = Settings()
+print(settings)
+print(type(settings))
